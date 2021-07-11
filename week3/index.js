45ab72ed5339442b0821397401986a845a7de32f
Listing <listing.zhao@gmail.com>
2021-07-11 11:30:35 +0800

    有向图拓扑BFS

--- week3/index.js
+++ week3/index.js
@@ -30,3 +30,71 @@
   }
   return build(0, inorder.length - 1, 0, postorder.length - 1);
 };
+
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+ let n = 0;
+ let edges = [] // [[], []]
+ let inDeg = [] // in degree 度数
+ 
+ const addEdges = (x, y) => {
+     edges[x].push(y)
+     inDeg[y]++
+ }
+ 
+ const topsort = () => {
+     let learned = []
+     // 拓扑排序 基于BFS需要队列
+     const q = [];
+     // 从所有零入度点出发
+     for (let i = 0; i < n; i++) {
+         if (inDeg[i] == 0) {
+             q.push(i)
+         }
+     }
+     // BFS
+     while (q.length) {
+         //取出队头，这门课程学了
+         const x = q.shift();
+         learned.unshift(x)
+         // 考虑x的所有出边
+         for (const y of edges[x]) {
+             inDeg[y]--; // 去掉约束关系
+             if (inDeg[y] == 0) {
+                 q.push(y)
+             }
+         }
+     }
+ 
+     if (n != learned.length) {
+         return []
+     }
+ 
+     return learned;
+ }
+ 
+ var findOrder = function (numCourses, prerequisites) {
+     n = numCourses;
+     edges = []
+     inDeg = []
+ 
+     // 初始化
+     for (let i = 0; i < n; i++) {
+         edges.push([])
+         inDeg[i] = 0
+     }
+ 
+     // 遍历
+     for (const pre of prerequisites) {
+         const ai = pre[0]
+         const bi = pre[1]
+         // 加边模板
+         addEdges(ai, bi)
+     }
+ 
+     return topsort();
+ };
